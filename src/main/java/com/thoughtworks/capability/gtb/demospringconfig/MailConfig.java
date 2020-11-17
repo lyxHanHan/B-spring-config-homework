c93e5cbfa5021e4e5a399c2a43bc286ac60c0b9e
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-17 22:36:21 +0800

    the solution of problem1

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,44 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+
+    public Map getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    private Map additionalHeaders;
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
+    public List getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    private Credentials credentials;
+    private List defaultRecipients;
 
     public String getHostname() {
         return hostname;
@@ -39,6 +71,10 @@ public class MailConfig {
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", defaultRedipients=" + defaultRecipients +
+                ", additionalHeaders=" + additionalHeaders +
+                ", credentials=" + " username: " + credentials.getUsername() + "password:"
+                + credentials.getPassword() + "authMethod:" + credentials.getAuthMethod()+
                 '}';
     }
 }
